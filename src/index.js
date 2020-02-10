104224c9e3601f3a6c53b143ca9624b627bcc651
eridho.rollian <eridho.rollian@e-consult.de>
2020-02-10 14:07:52 +0100

    deleted unused app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import AppModal from "./main";
 import * as serviceWorker from "./serviceWorker";
 
